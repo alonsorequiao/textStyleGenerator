c2078c3dd7d10b8b82ef3b850f354e5fa05a63dd
Pedro Alonso <alonsofts@gmail.com>
2020-05-16 17:58:38 -0400

    New feature - Now you can see the styles you chose not only in console but on the main screen

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ var mySet_text = [];
 var mySet_color = [];
 var mySet_fontSize = [];
 
+var choiceContainerDisplay = document.getElementById("choiceContainer");
+
+var countSaved;
+
 function changeColorh1() {
     header.style.color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
     header2.innerText = header.style.color.toUpperCase();
@@ -23,12 +27,14 @@ textBox.onkeyup = function() {
 
 function saveSet() {
 
+    choiceContainerDisplay.innerHTML= "";
+
     mySet_text.push(header.textContent);
     mySet_color.push(header.style.color.toUpperCase());
     mySet_fontSize.push(header.style.fontSize);
 
+    // CONSOLE
     console.log("Tamanho do Array: " + mySet_text.length + "\n");
-
     for(var i = 0; i < mySet_text.length; i++ ) {
         console.log("No FOR, i="
             + i
@@ -39,6 +45,26 @@ function saveSet() {
             + "\n");
     }
     console.log("Final");
+
+    //Print on ChoiceContainer
+    // for (i = 0; i < mySet_text.length; i++) {
+    //     console.log("Teste " + i);
+    //     choiceContainerDisplay.innerHTML +=
+    //         mySet_text[i]
+    //         + "<br>"
+    //         + mySet_color[i]
+    //         + "<br>"
+    //         + mySet_fontSize[i]
+    //         + "<br><br>";
+    // }
+
+    for (i = 0; i < mySet_text.length; i++) {
+        console.log("Teste " + i);
+        choiceContainerDisplay.innerHTML +=
+        // "<style = 'font-size:'" + mySet_fontSize + ">" + mySet_text[i] + "</style>";
+            "<span style='font-size:" + mySet_fontSize[i] + ";" + " color:" + mySet_color[i] + "'>" + mySet_text[i] + "</span> <br>";
+
+    }
 }
 
 
